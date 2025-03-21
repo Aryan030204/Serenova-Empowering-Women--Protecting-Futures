5a20b7bfc1078d8a78e224ccb8dfed49dcd95f6e
aryan22csu031 <aryan22csu031@ncuindia.edu>
2025-03-21 12:06:29 +0530

    updated

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,2 +1,5 @@
 export const SERVER_URL="http://localhost:3000/api"
 export const PYTHON_APP_URL="http://127.0.0.1:5000"
+export const EMAIL_TEMPLATE_ID="template_aht6na8"
+export const EMAIL_SERVICE_ID="service_1o4ehaj"
+export const EMAIL_PUBLIC_KEY="NcCPchw9egU4lcpDp"
